309d27a85a7e09bf395a517309cb627540c913fe
brian.muteti <brian.muteti@equitybank.co.ke>
2026-03-24 11:50:47 +0300

    Initial Commit

---
+++ src/test/java/com/gag/transaction_lib/TransactionLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gag.transaction_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
